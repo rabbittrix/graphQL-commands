31a6b2ceb5cf8bed55c57625e2f6013bca1e8130
carcloud <rabbittrix@hotmail.com>
2020-03-26 21:53:05 +0000

    enum status

--- data/db.js
+++ data/db.js
@@ -1,23 +1,33 @@
-const users = [{
-  id: 1,
-  name: 'James Smith',
-  email: 'james@gamil.com',
-  age: 29,
-  perfil_id: 1
-},{
-  id: 2,
-  name: 'Rafael filho',
-  email: 'rafael@gamil.com',
-  age: 22,
-  perfil_id: 2
-},{
-  id: 3,
-  name: 'Jessica Lane',
-  email: 'jessica@gamil.com',
-  age: 34,
-  perfil_id: 1
-}]
+const users = [
+  {
+    id: 1,
+    name: "James Smith",
+    email: "james@gamil.com",
+    age: 29,
+    perfil_id: 1,
+    status: "ACTIVE"
+  },
+  {
+    id: 2,
+    name: "Rafael filho",
+    email: "rafael@gamil.com",
+    age: 22,
+    perfil_id: 2,
+    status: "INACTIVE"
+  },
+  {
+    id: 3,
+    name: "Jessica Lane",
+    email: "jessica@gamil.com",
+    age: 34,
+    perfil_id: 1,
+    status: "BLOCKED"
+  }
+];
 
-const perfis = [{id: 1, name:'Customer'}, {id: 2, name: 'Admin'}]
+const perfis = [
+  { id: 1, name: "Customer" },
+  { id: 2, name: "Admin" }
+];
 
-module.exports = {users, perfis}
+module.exports = { users, perfis };
